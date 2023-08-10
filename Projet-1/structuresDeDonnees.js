869efb598c7df63796adc5f0ce3d7a0e024aae14
pierre lambert <pierrelambert41@gmail.com>
2023-08-10 15:10:24 -0400

    add LinkedList

--- Projet-1/structuresDeDonnees.js
+++ Projet-1/structuresDeDonnees.js
@@ -69,20 +69,144 @@ class LinkedList {
             this.tail = null
         }
         temp.next = null
+        console.log('TEMP ', temp)
         return temp
     }
+
+    unshift(value) {
+        const newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(value) {
+        let current = this.head;
+        let currentIndex = 0;
+
+        while (current !== null) {
+            if (current.value === value) {
+                return currentIndex;
+            }
+            current = current.next;
+            currentIndex++;
+        }
+
+        return -1;
+    }
+
+    getIndexOf(position) {
+        if (position < 0 || position >= this.length ) {
+            return undefined;
+        }
+        let current = this.head;
+        let currentIndex = 0;
+
+        while (currentIndex < position) {
+            current = current.next;
+            currentIndex++
+        }
+
+        return current;
+    }
+
+    /* insert(value, position) {
+        const newNode = new Node(value);
+        if (position === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            let currentPosition = 0;
+            let previous = null;
+
+            while (currentPosition < position && current !== null ) {
+                previous = current;
+                current = current.next;
+                currentPosition++;
+            }
+
+            if (currentPosition < position) {
+                previous.next = newNode;
+            } else {
+                previous.next = newNode;
+                newNode.next = current;
+            }
+        }
+        this.length++
+    } */
+
+    insert(position, value){
+        if(position === 0) return this.unshift(value);
+        if(position === this.length) return this.push(value)
+
+        const newNode = new Node(value)
+        const temp = this.getIndexOf(position - 1)
+
+        newNode.next = temp.next
+        temp.next = newNode
+        this.length++
+        return true
+    }
+
+    remove(position) {
+        if (position < 0 || position > this.length ) {
+            return undefined;
+        }
+
+        if (position === 0) return this.shift();
+        if (position === this.length) return this.pop();
+        const previous = this.getIndexOf(position - 1);
+        let current = previous.next;
+        previous.next = current.next;
+        
+        this.length--
+
+    }
+
+    reverse() {
+        let current = this.head;
+        let previous = null;
+        let nextNode = null;
+
+        while (current !== null) {
+            nextNode = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextNode;
+        }
+
+        this.head = previous;
+    }
+
 
 }
 
 let myLinkedList = new LinkedList(14);
-console.log('beforePush', myLinkedList)
 
 myLinkedList.push(1);
 myLinkedList.push(15);
 myLinkedList.push(12);
 myLinkedList.push(17);
 
-console.log('afterPush', myLinkedList)
+/* console.log('afterPush', myLinkedList)
+// console.log('afterPush2', JSON.stringify(myLinkedList, null, 2));
 myLinkedList.shift();
 console.log('AfterShift ', myLinkedList)
+myLinkedList.unshift(12);
+console.log('GET ', myLinkedList.get(12))
+console.log('GET ', myLinkedList.getIndexOf(0))
+console.log('unshift ', myLinkedList)
+myLinkedList.insert(2, 0)
+console.log('insert0 ', myLinkedList)
+myLinkedList.remove(-60) */
+console.log('LinkedList', JSON.stringify(myLinkedList, null, 2));
+myLinkedList.reverse()
+console.log('reverse', JSON.stringify(myLinkedList, null, 2));
 
